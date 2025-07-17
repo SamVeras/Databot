f6a2774708d624bf57dd487a8d4d6de4ef756b1b
SamVeras <samuel.bernardo.veras@gmail.com>
2025-07-17 01:17:08 -0300

    novo scrape to-do

--- cogs/scrape_commands.py
+++ cogs/scrape_commands.py
@@ -1,76 +1,129 @@
 from discord.ext import commands
-import csv
+import logging
+import pymongo
 from datetime import datetime
-import os
+from config import MONGO_URI
 
 
 class ScrapeCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.mongo_client = pymongo.MongoClient(MONGO_URI)
+        self.db = self.mongo_client["discord_data"]
+        self.collection = self.db["messages"]
 
-    async def _scrape_channel(self, ctx, silent=False):
-        """Internal method to scrape channel data"""
-        channel = ctx.channel
-        guild = ctx.guild
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        filename = f"scraped_data_{guild.name}_{channel.name}_{timestamp}.csv"
-        filename = "".join(c for c in filename if c.isalnum() or c in (" ", "-", "_", ".")).rstrip()
+    @staticmethod
+    async def message_to_dict(message):
+        return {
+            "id": getattr(message, "id", None),
+            "author": ({"id": message.author.id, "name": message.author.name}),
+            "content_raw": message.content,
+            "content_clean": message.clean_content,
+            "creation_date": message.created_at,
+            "edit_date": getattr(message, "edited_at", None),
+            "is_pinned": message.pinned,
+            "channel": {
+                "id": getattr(message.channel, "id", None),
+                "name": getattr(message.channel, "name", None),
+            },
+            "guild": {
+                "id": getattr(message.guild, "id", None),
+                "name": getattr(message.guild, "name", None),
+            },
+            "reference": (
+                {
+                    "id": getattr(message.reference, "message_id", None),
+                    "channel_id": getattr(message.reference, "channel_id", None),
+                    "guild_id": getattr(message.reference, "guild_id", None),
+                }
+            ),
+            "embeds": [
+                {
+                    "title": embed.title,
+                    "type": embed.type,
+                    "description": embed.description,
+                    "url": embed.url,
+                }
+                for embed in message.embeds
+            ],
+            "reactions": [
+                {
+                    "emoji": reaction.emoji,
+                    "count": reaction.count,
+                }
+                for reaction in message.reactions
+            ],
+            "mentions": [
+                {
+                    "id": user.id,
+                    "name": user.name,
+                }
+                for user in message.mentions
+            ],
+            "mentions_roles": [
+                {
+                    "id": role.id,
+                    "name": role.name,
+                    "color": getattr(role.color, "value", None),
+                }
+                for role in message.role_mentions
+            ],
+            "stickers": [
+                {
+                    "id": sticker.id,
+                    "name": sticker.name,
+                    "url": getattr(sticker, "url", None),
+                    "format": getattr(sticker, "format", None),
+                }
+                for sticker in message.stickers
+            ],
+            "attachments": [
+                {
+                    "id": attachment.id,
+                    "url": getattr(attachment, "url", None),
+                    "filename": getattr(attachment, "filename", None),
+                    "content_type": getattr(attachment, "content_type", None),
+                    "spoiler": attachment.is_spoiler(),
+                    "size": getattr(attachment, "size", None),
+                    "width": getattr(attachment, "width", None),
+                    "height": getattr(attachment, "height", None),
+                    "duration_secs": getattr(attachment, "duration_secs", None),
+                }
+                for attachment in message.attachments
+            ],
+            "poll": (
+                {
+                    "question": getattr(message.poll, "question", None),  # str
+                    "duration": getattr(message.poll, "duration", None),  # datetime.timedelta
+                    "multiple_choice": getattr(message.poll, "multiple", None),  # bool
+                    "answers": [
+                        {
+                            "id": getattr(answer, "id", None),  # int
+                            "emoji": getattr(answer, "emoji", None),  # str
+                            "text": getattr(answer, "text", None),  # str
+                            "count": getattr(answer, "vote_count", None),  # int
+                            "victor": getattr(answer, "victor", None),  # bool
+                            "voters": [
+                                {
+                                    "id": getattr(voter, "id", None),  # int
+                                    "name": getattr(voter, "name", None),  # str
+                                }
+                                async for voter in answer.voters()
+                            ],  # list
+                        }
+                        for answer in message.poll.answers
+                    ],
+                }
+                if hasattr(message, "poll") and message.poll
+                else None
+            ),
+        }
 
-        os.makedirs("scraped_data", exist_ok=True)
-        filepath = os.path.join("scraped_data", filename)
-
-        print(f"Iniciando coleta de dados do canal {channel.name}...")
-        if not silent:
-            await ctx.send(f"Iniciando coleta de dados do canal {channel.name}...")
-
-        with open(filepath, "w", newline="", encoding="utf-8") as csvfile:
-            writer = csv.DictWriter(
-                csvfile,
-                fieldnames=[
-                    "timestamp",
-                    "author_id",
-                    "author_name",
-                    "author_discriminator",
-                    "message_id",
-                    "content",
-                    "channel_name",
-                    "guild_name",
-                ],
-            )
-            writer.writeheader()
-
-            message_count = 0
-            async for message in channel.history(limit=None, oldest_first=True):
-                content = message.content.replace('"', '""')
-                content = message.content.replace("\n", "\\n")
-
-                writer.writerow(
-                    {
-                        "timestamp": message.created_at.isoformat(),
-                        "author_id": message.author.id,
-                        "author_name": message.author.name,
-                        "author_discriminator": message.author.discriminator,
-                        "message_id": message.id,
-                        "content": content,
-                        "channel_name": channel.name,
-                        "guild_name": guild.name,
-                    }
-                )
-                message_count += 1
-
-                if message_count % 100 == 0:
-                    print(f"{message_count} mensagens processadas...")
-
-            print(f"{message_count} mensagens salvas em {filepath}.")
-        if not silent:
-            await ctx.send(f"{message_count} mensagens salvas em {filepath}.")
-
-    @commands.hybrid_command(name="scrape", description="Coletar dados")
+    @commands.hybrid_command(name="scrape", description="Coletar dados do canal.")
     @commands.has_permissions(administrator=True)
-    async def scrape(self, ctx):
-        await self._scrape_channel(ctx, silent=False)
-
-    @commands.hybrid_command(name="scrapesilent", description="Coletar dados silenciosamente")
-    @commands.has_permissions(administrator=True)
-    async def scrapesilent(self, ctx):
-        await self._scrape_channel(ctx, silent=True)
+    async def scrape_channel(self, ctx):
+        """Coletar dados do canal e guarda em um arquivo."""
+        logging.info(f"Coletando dados do canal {ctx.channel.name}...")
+        await ctx.send(f"Coletando dados do canal {ctx.channel.name}...")
+        # to-do, fazer depois
+        pass
--- config.py
+++ config.py
@@ -4,4 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD_ID = os.getenv("GUILD_ID")
+MONGO_URI = os.getenv("MONGO_URI")
 BOT_PREFIX = "~"
